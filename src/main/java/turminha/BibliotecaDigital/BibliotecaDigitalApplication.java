def5b94ea8ecc713437a086b2ece95923796b924
amdre <andrenfranca1@gmail.com>
2026-03-21 19:26:10 -0300

    Projeto inicial

---
+++ src/main/java/turminha/BibliotecaDigital/BibliotecaDigitalApplication.java
@@ -0,0 +1,13 @@
+package turminha.BibliotecaDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDigitalApplication.class, args);
+	}
+
+}
