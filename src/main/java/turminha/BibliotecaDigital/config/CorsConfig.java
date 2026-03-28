ef8c0f382b59ab95b8e9f0ff1953edb3cd639af1
amdre <andrenfranca1@gmail.com>
2026-03-27 23:12:01 -0300

    Configuração config, front-end ta chegando ein tropinha

--- src/main/java/turminha/BibliotecaDigital/config/CorsConfig.java
+++ src/main/java/turminha/BibliotecaDigital/config/CorsConfig.java
@@ -1,4 +1,21 @@
 package turminha.BibliotecaDigital.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")           // libera todas as portas aq o bgl é loco
+                        .allowedOrigins("http://localhost:3000") // só esse front pode chamar
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+            }
+        };
+    }
 }
